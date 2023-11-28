18e47a3566ee64b3fa7c79e270df54f8bfeeeb8b
ZombieHeroX <Alan2390690731@gmail.com>
2023-11-28 17:17:21 -0500

    Code

--- P1.cpp
+++ P1.cpp
@@ -1,8 +1,40 @@
 //include headers and libraries
+#include "Library.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
 
 int main(){
     Library library;
     
     //ask for commands and execute
-
+    string command;
+    do{
+        cout<<"Enter command: "<<endl;
+	    cin>>command;
+        if(command == "a"){
+            string bookName;
+            cin >> bookName;
+            if(library.addBook(bookName)){
+                //cout << "success" << endl;
+                ;
+            }else{
+                cout << "Cannot add the book."<< endl;
+            }
+        }else if(command == "r"){
+            string bookName;
+            cin >> bookName;
+            if(library.removeBook(bookName)){
+                //cout << "success" << endl;
+                ;
+            }else{
+                cout << "Cannot remove the book." << endl;
+            }
+        }else if(command == "p"){
+            library.print();
+        }
+    }while(command != "q");
+    return 0;
 }
